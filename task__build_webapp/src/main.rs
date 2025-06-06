988d3ba1314145a44d2cf17ada95d56f6ad42bf4
Tomaz Vieira <tom.fogaca@gmail.com>
2025-06-06 14:57:28 +0000

    Adds task crate to build webapp. Use via `cargo run -p task__build_webapp`

---
+++ task__build_webapp/src/main.rs
@@ -0,0 +1,31 @@
+use std::path::PathBuf;
+
+// Example custom build script.
+fn main() {
+    let workspace_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).parent().unwrap().to_owned();
+    let docs_dir = {
+        let mut dir = workspace_dir.clone();
+        dir.push("docs");
+        dir
+    };
+    let docs_gitignore = {
+        let mut dir = docs_dir.clone();
+        dir.push(".gitignore");
+        dir
+    };
+    let bioimg_gui_dir = {
+        let mut dir = workspace_dir.clone();
+        dir.push("bioimg_gui");
+        dir
+    };
+
+    let a = std::process::Command::new("trunk")
+        .current_dir(&bioimg_gui_dir)
+        .arg("build")
+        .arg("--release=true")
+        .arg(format!("--dist={}", docs_dir.to_string_lossy()))
+        .output().expect("Expected trunk to run");
+
+    //de-ignore the docs dir so that it can be committed to the gh-pages branch
+    std::fs::write(&docs_gitignore, "!*").unwrap();
+}
