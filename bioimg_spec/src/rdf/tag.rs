23bec0afe394ab9cacf353dcbe208fdc66d0b324
Tomaz Vieira <tom.fogaca@gmail.com>
2025-06-30 12:19:27 +0000

    impl AsPartial for Tag

--- bioimg_spec/src/rdf/tag.rs
+++ bioimg_spec/src/rdf/tag.rs
@@ -1,10 +1,16 @@
 use std::{borrow::Borrow, str::FromStr};
 
+use crate::util::AsPartial;
+
 use super::{bounded_string::BoundedStringParsingError, BoundedString};
 
 
 #[derive(serde::Deserialize, serde::Serialize, Clone, Debug, PartialEq, Eq)]
 pub struct Tag(BoundedString<1, 1024>);
+
+impl AsPartial for Tag{
+    type Partial = String;
+}
 
 impl Borrow<str> for Tag{
     fn borrow(&self) -> &str {
